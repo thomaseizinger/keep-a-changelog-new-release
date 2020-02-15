68d4b826dbac1e3b3f99e8dc95e3fb1f2b94db05
Thomas Eizinger <thomas@eizinger.io>
2020-02-15 21:22:48 +0800

    Make sure genesishash is valid hex

--- __tests__/getGenesisHash.test.ts
+++ __tests__/getGenesisHash.test.ts
@@ -4,4 +4,5 @@ it("should return a hash", async function() {
   const hash = await getGenesisHash();
 
   expect(hash).toHaveLength(40);
+  expect(hash).toMatch(/[a-f0-9]+/) // must be valid hex
 });
